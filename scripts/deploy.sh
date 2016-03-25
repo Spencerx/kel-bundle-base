05182e7d0fe09472ad34f4636d184315321f723a
Brian Rosner <brosner@eldarion.com>
2016-03-24 20:13:25 -0600

    Force latest tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ set -ev
 . $(dirname $(readlink -f $0))/common.sh
 
 docker login -e noemail -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-docker tag "$REPO:$BUILD_TAG" "$REPO:latest"
+docker tag -f "$REPO:$BUILD_TAG" "$REPO:latest"
 docker push "$REPO:$BUILD_TAG"
 docker push "$REPO:latest"
